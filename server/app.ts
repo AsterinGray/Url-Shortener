ea9931bf194678428522e7b939fa7ff02e86c6b9
AsterinGray <raymondtiandinata@gmail.com>
2021-12-11 23:58:55 +0700

    refactor(server db): change database url to env

--- server/app.ts
+++ server/app.ts
@@ -2,14 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-import { PORT } from './app/config'
+import { MONGODB_URI, PORT } from './app/config'
 import routes from './app/routes'
 
 const app = express()
 
-mongoose.connect(
-  'mongodb+srv://AsterinGray:ProjectAG19@urlshortener.msu6e.mongodb.net/URLShortener?retryWrites=true&w=majority'
-)
+mongoose.connect(MONGODB_URI)
 
 const db = mongoose.connection
 db.on('error', (err) => console.error(err))
